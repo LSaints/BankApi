463937b78950b097b26a32ed530cdd67b5d64801
Mateus <mateus@pop-os.localdomain>
2024-04-07 09:18:51 -0300

    refactor: Removido espaços em branco

--- src/BankApplication.Domain/Entities/Transaction.cs
+++ src/BankApplication.Domain/Entities/Transaction.cs
@@ -6,7 +6,7 @@ public class Transaction
     public Guid SenderId {get; set;}
     public User Sender {get; set;}
     public User Receiver {get; set;}
-     public Guid ReceiverId {get; set;}
+    public Guid ReceiverId {get; set;}
     public double Amount {get; set;}
     public string? Description {get; set;}
     public DateTime Date {get; set;}

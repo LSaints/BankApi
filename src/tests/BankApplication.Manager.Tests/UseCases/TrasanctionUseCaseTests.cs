b3abb194b2fbadbdc1e58a0e551e928b205c81f1
Mateus <mateus@pop-os.localdomain>
2024-04-08 16:35:53 -0300

    feat: Adicionar arquivos de testes & pipeline para o jenkins

---
+++ src/tests/BankApplication.Manager.Tests/UseCases/TrasanctionUseCaseTests.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Moq;
+
+namespace BankApplication.Manager.Tests;
+
+public class TrasanctionUseCaseTests
+{
+    private TransactionUseCase _useCase;
+    private readonly Mock<ITransactionRepository> _repositoryMock;
+
+    public TrasanctionUseCaseTests()
+    {
+        _repositoryMock = new Mock<ITransactionRepository>();
+        _useCase = new TransactionUseCase(_repositoryMock.Object, new Mock<IMapper>().Object);
+    }
+}
